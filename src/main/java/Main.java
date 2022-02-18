b6cee25bcd8168ea67c9b182ba8c4d47c591fc5f
kim-dongoh <dongoh31277@gmail.com>
2022-02-18 12:45:58 +0900

    connect

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        connection connection = new connection();
+        Scanner scanner = new Scanner(System.in);
+
+        String IP;
+        String DB;
+        String USER;
+        String PASSWORD;
+
+        //Postgresql Connection
+        while (true) {
+            while (true) {
+                System.out.print("Enter IP: ");
+                IP = scanner.nextLine();
+
+                if (!IP.isEmpty()) {
+                    break;
+                }
+            }
+
+            while (true) {
+                System.out.print("Enter DB: ");
+                DB = scanner.nextLine();
+
+                if (!DB.isEmpty()) {
+                    break;
+                }
+            }
+
+            while (true) {
+                System.out.print("Enter USER: ");
+                USER = scanner.nextLine();
+
+                if (!USER.isEmpty()) {
+                    break;
+                }
+            }
+
+            while (true) {
+                System.out.print("Enter PASSWORD: ");
+                PASSWORD = scanner.nextLine();
+
+                if (!PASSWORD.isEmpty()) {
+                    break;
+                }
+            }
+
+            connection.createConnection(IP, DB, USER, PASSWORD);
+            break;
+        }
     }
 }
--- src/main/java/connection.java
+++ src/main/java/connection.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class connection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connection {
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+    private static Connection connection;
+
+    public void createConnection(String IP, String DB, String USER, String PASSWORD) {
+        String URL = "jdbc:postgresql://" + IP + "/" + DB;
+
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connect!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
