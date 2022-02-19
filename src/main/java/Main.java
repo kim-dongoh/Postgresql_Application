67194981ea052c6ee2e723e522126afdf53021f1
kim-dongoh <dongoh31277@gmail.com>
2022-02-19 20:18:36 +0900

    modify connection class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,13 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         connection connection = new connection();
-        Scanner scanner = new Scanner(System.in);
 
-        String IP;
-        String DB;
-        String USER;
-        String PASSWORD;
+        connection.createConnection();
+        connection.getConnection();
 
-        //Postgresql Connection
-        while (true) {
-            while (true) {
-                System.out.print("Enter IP: ");
-                IP = scanner.nextLine();
 
-                if (!IP.isEmpty()) {
-                    break;
-                }
-            }
-
-            while (true) {
-                System.out.print("Enter DB: ");
-                DB = scanner.nextLine();
-
-                if (!DB.isEmpty()) {
-                    break;
-                }
-            }
-
-            while (true) {
-                System.out.print("Enter USER: ");
-                USER = scanner.nextLine();
-
-                if (!USER.isEmpty()) {
-                    break;
-                }
-            }
-
-            while (true) {
-                System.out.print("Enter PASSWORD: ");
-                PASSWORD = scanner.nextLine();
-
-                if (!PASSWORD.isEmpty()) {
-                    break;
-                }
-            }
-
-            connection.createConnection(IP, DB, USER, PASSWORD);
-            break;
-        }
     }
 }
--- src/main/java/connection.java
+++ src/main/java/connection.java
@@ -1,21 +1,56 @@
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class connection {
-    private static String URL;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static Connection connection;
+    private static String IP;
+    private static String DB;
     private static String USER;
     private static String PASSWORD;
-    private static Connection connection;
 
-    public void createConnection(String IP, String DB, String USER, String PASSWORD) {
-        String URL = "jdbc:postgresql://" + IP + "/" + DB;
+    public void createConnection() throws IOException {
+        while (IP=="") {
+            bw.write("Enter IP: ");
+            bw.flush();
+            IP = br.readLine();
+        }
+
+        while (DB=="") {
+            bw.write("Enter DB: ");
+            bw.flush();
+            DB = br.readLine();
+        }
+
+        while (USER=="") {
+            bw.write("Enter USER: ");
+            bw.flush();
+            USER = br.readLine();
+        }
+
+        while (PASSWORD=="") {
+            bw.write("Enter PASSWORD: ");
+            bw.flush();
+            PASSWORD = br.readLine();
+        }
 
         try {
+            String URL = "jdbc:postgresql://" + IP + "/" + DB;
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connect!");
-        } catch (SQLException e) {
+
+            bw.write("Connect!");
+            bw.flush();
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
+        bw.close();
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
