8f94c0f226446a7dc3810a27adb0d3cf1b54eb08
kim-dongoh <dongoh31277@gmail.com>
2022-02-26 21:13:37 +0900

    data

--- src/main/java/data.java
+++ src/main/java/data.java
@@ -14,16 +14,29 @@ public class data {
             if (select == 1) {
                 insertData(connection);
             } else if (select == 2) {
-
+                updatetData(connection);
             } else if (select == 3) {
-
+                deleteData(connection);
             } else if (select == 4) {
                 break;
             }
         }
     }
 
-    private void insertData (Connection connection) {
+    private void insertData (Connection connection) throws IOException {
+        bw.write("\nInput your table name: ");
+        bw.flush();
+        String TABLE_NAME = br.readLine();
+
+        //테이블 칼럼명 호출
+        String sql = "SELECT column_name FROM information_schema.columns WHERE table_name = ' "+ TABLE_NAME +"';";
+        String sql2 = "SELECT udt_name FROM information_schema.columns WHERE table_name = ' "+ TABLE_NAME +"';";
+        String sql3 = "SELECT character_maximum_length FROM information_schema.columns WHERE table_name = ' "+ TABLE_NAME +"';";
+    }
+    private void updatetData (Connection connection) {
+        String  TABLE_NAME;
+    }
+    private void deleteData (Connection connection) {
         String  TABLE_NAME;
     }
 }
